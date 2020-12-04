f1cbd9f0f1ee919690963b3ad87f5d4d984ba5c9
FRANK <Frank1126.lin@gmail.com>
2020-12-04 16:57:51 +0800

    Update predict.py

--- yolov5_tcp_deploy/predict.py
+++ yolov5_tcp_deploy/predict.py
@@ -17,11 +17,13 @@ import cv2
 from models.experimental import attempt_load
 from utils.torch_utils import select_device, time_synchronized
 from utils.datasets import LoadImages
-from utils.utils import check_img_size, non_max_suppression, scale_coords, xyxy2xywh, plot_one_box
+from utils.general import check_img_size, non_max_suppression, scale_coords, xyxy2xywh
+from utils.plots import plot_one_box
 
 
 yolo_config = json.load(open("yolov5_config.json", "r"))
-print(yolo_config)
+for k,v in yolo_config.items():
+    print(k,":", v)
 weights = yolo_config["weights"]
 source = yolo_config["source"]
 save_dir = yolo_config["save-dir"]
@@ -33,7 +35,7 @@ classes = yolo_config["classes"] if yolo_config["classes"] != "all" else None
 
 
 def predict( save_img = True, weights = weights, source = source, out = save_dir, imgsz = img_size, conf_thres = conf_thres,
-            iou_thres = iou_thres,device = device,save_txt = True,classes = classes,agnostic_nms = False,):
+            iou_thres = iou_thres, device = device, save_txt = True, classes = classes, agnostic_nms = False,):
     # Initialize
     device = select_device(device)
     half = device.type != 'cpu'  # half precision only supported on CUDA
@@ -51,7 +53,6 @@ def predict( save_img = True, weights = weights, source = source, out = save_dir
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
 
     # Run inference
-    t0 = time.time()
     img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
     _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
     for path, img, im0s, vid_cap in dataset:
@@ -62,52 +63,34 @@ def predict( save_img = True, weights = weights, source = source, out = save_dir
             img = img.unsqueeze(0)
 
         # Inference
-        t1 = time_synchronized()
         pred = model(img)[0]
 
         # Apply NMS
         pred = non_max_suppression(pred, conf_thres, iou_thres, classes=classes, agnostic=agnostic_nms)
-        t2 = time_synchronized()
-        rst_txt = ''
 
         # Process detections
-        i, det = pred # detections per image === single img
         p, s, im0 = path, '', im0s
-        save_path = str(Path(out) / Path(p).name)
-        txt_path = str(Path(out) / Path(p).stem)
-        s += '%gx%g \n' % img.shape[2:]  # print string
-        if det is not None and len(det):
-            # Rescale boxes from img_size to im0 size
-            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
-            # Print and save results
-            for c in det[:, -1].unique():
-                n = (det[:, -1] == c).sum()  # detections per class
-                s += '%g %s, ' % (n, names[int(c)])  # add to string
+        ss = {"imgPath":source} # dict format
+        save_path = str(save_dir / p.name)
+        for i, det in enumerate(pred): # detections in image
 
-
-            for *xyxy, conf, cls in reversed(det):
-                # if save_txt:  # Write to file
-                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                line = (cls, conf, *xywh) if opt.save_conf else (cls, *xywh)  # label format
-                rst_txt += line * len(line) + '\n'
-                with open(txt_path + '.txt', 'a') as f:
-                    f.write(rst_txt)
-
-            # Plot box
-            for *xyxy, conf, cls in det:
-                label =  "{}:{:.2f}%".format(names[int(cls)], conf*100)
-                plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=2)
-
-        if save_img and dataset.mode == 'images':
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                for *xyxy, conf, cls in det:
+                    xyxy = torch.tensor(xyxy).view(1,4).view(-1).tolist()
+                    conf = conf.item()
+                    obj = {"label": names[int(cls)], "conf": conf, "points": xyxy}
+                    ss[i] = obj
+                    label =  "{}:{:.2f}%".format(names[int(cls)], conf*100)
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=2)
+        if save_img:
             cv2.imwrite(save_path, im0)
 
-        # Print time (inference + NMS)
-        print(f'%sDone. (%.3fs)' % (s, t2 - t1))
-        print('Results saved to %s' % Path(out))
+        return ss, im0
 
-        return rst_txt, im0
+# if __name__ == '__main__':
+#     with torch.no_grad():
+#             predict()
 
-if __name__ == '__main__':
-    with torch.no_grad():
-            predict()
